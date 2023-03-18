9d541df33fc4334a978d68ba891e05a652268e67
amiralam0 <sharpprogrammer2018@gmail.com>
2023-03-19 01:39:03 +0530

    Update SingleEvent.tsx

--- client/src/Components/SingleEvent.tsx
+++ client/src/Components/SingleEvent.tsx
@@ -42,14 +42,25 @@ export const SingleEvent = () => {
             </Title>
           </Flex>
           <Text>A hackathon where bla bla bla bla bla bla bla</Text>
-          <Flex style={{ marginRight: 'auto' }}>
-            <ActionIcon color={'green'}>
-              <IconArrowBigUp size={16} />
-            </ActionIcon>
-            <ActionIcon color={'red'}>
-              <IconArrowBigDown size={16} />
-            </ActionIcon>
-          </Flex>
+          <Group mt="sm" spacing="xs">
+            <Button
+              size="sm"
+              leftIcon={<IconArrowBigUp size={22} />}
+              color={status === 'upvoted' ? 'blue' : 'gray'}
+              variant={status === 'upvoted' ? 'light' : 'subtle'}
+            >
+              {' '}Upvote{' '}
+            </Button>
+
+            <Button
+              size="sm"
+              leftIcon={<IconArrowBigDown size={22} />}
+              variant={status === 'downvoted' ? 'light' : 'subtle'}
+              color={status === 'downvoted' ? 'red' : 'gray'}
+            >
+              {' '}Downvote{' '}
+            </Button>
+          </Group>
         </Box>
       </Flex>
     </Paper>
