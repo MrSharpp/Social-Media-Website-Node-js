2fad89f3d83910836e32f8ca807150419fe1d48b
amiralam0 <sharpprogrammer2018@gmail.com>
2023-03-19 01:04:40 +0530

    Update AddFeed.tsx

--- client/src/Components/AddFeed.tsx
+++ client/src/Components/AddFeed.tsx
@@ -1,45 +1,81 @@
-import {Box, Paper, Grid, Avatar, Flex, Input, Group, Button } from '@mantine/core';
+import {
+  Box,
+  Paper,
+  Grid,
+  Avatar,
+  Flex,
+  Input,
+  Group,
+  Button,
+} from '@mantine/core';
 import {
   IconCamera,
-IconPhoto,
-IconVideo,
-IconFiles,
-IconMapPin,
-} from '@tabler/icons-react'
+  IconPhoto,
+  IconVideo,
+  IconFiles,
+  IconMapPin,
+} from '@tabler/icons-react';
 
 export const AddFeed = () => {
   return (
     <Paper shadow="xs" radius={'md'}>
-      <Flex p="xs"  gap="sm" >
-          <Avatar radius={'md'} mr={0} size={44} />
+      <Flex p="xs" gap="sm">
+        <Avatar radius={'md'} mr={0} size={44} />
 
-
-<Box w="100%" >
-
-            <Input variant='filled'
+        <Box w="100%">
+          <Input
+            variant="filled"
             size="md"
-             placeholder="Share or ask something from everyone" 
-             style={{ width: '100%' }} 
+            placeholder="Share or ask something from everyone"
+            style={{ width: '100%' }}
             styles={theme => ({
               input: {
-                background: theme.colors.gray[0], 
+                background: theme.colors.gray[0],
                 '&:hover': {
-                  background: theme.colors.gray[1]
-                }
-              }
- })} />
+                  background: theme.colors.gray[1],
+                },
+              },
+            })}
+          />
 
-
-          
-          <Group mt="sm" >
-            <Button size="sm" leftIcon={<IconCamera size={22} />}  color="gray" variant="subtle" > Camera </Button>
-            <Button size="sm" leftIcon={<IconPhoto size={22} />} color="gray" variant="subtle" > Images </Button>
-            <Button size="sm" leftIcon={<IconVideo size={22} />} color="gray" variant="subtle" > Videos </Button>
-            <Button size="sm" leftIcon={<IconFiles size={22} />} color="gray" variant="subtle" > Files </Button>
-            <Button size="sm" leftIcon={<IconMapPin size={22} />} color="gray" variant="subtle" > Location </Button>
-
-            </Group>
- </Box>
+          <Group mt="sm">
+            <Button
+              size="sm"
+              leftIcon={<IconCamera size={22} />}
+              color="gray"
+              variant="subtle"
+            >
+              {' '}Camera{' '}
+            </Button>
+            <Button
+              size="sm"
+              leftIcon={<IconPhoto size={22} />}
+              color="gray"
+              variant="subtle"
+            >
+              {' '}Images{' '}
+            </Button>
+            <Button
+              size="sm"
+              leftIcon={<IconVideo size={22} />}
+              color="gray"
+              variant="subtle"
+            >
+              {' '}Videos{' '}
+            </Button>
+            <Button
+              size="sm"
+              leftIcon={<IconFiles size={22} />}
+              color="gray"
+              variant="subtle"
+            >
+              {' '}Files{' '}
+            </Button>
+            <Button size="sm" color="blue" variant="filled">
+              {' '}Create Post{' '}
+            </Button>
+          </Group>
+        </Box>
       </Flex>
     </Paper>
   );
