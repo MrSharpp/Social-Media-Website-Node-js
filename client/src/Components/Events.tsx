9d175a07327ebaf2d7ac7655da4f070b32082131
amiralam0 <sharpprogrammer2018@gmail.com>
2023-03-19 12:47:08 +0530

    recent events

--- client/src/Components/Events.tsx
+++ client/src/Components/Events.tsx
@@ -1,20 +1,57 @@
-import { Anchor, Flex, Paper, Text } from '@mantine/core';
+import {
+  Anchor,
+  Flex,
+  Paper,
+  Text,
+  Title,
+  createStyles,
+  rem,
+} from '@mantine/core';
+
+const useStyles = createStyles(theme => ({
+  card: {
+    position: 'relative',
+    cursor: 'pointer',
+    overflow: 'hidden',
+    transition: 'transform 150ms ease, box-shadow 100ms ease',
+    padding: theme.spacing.xs,
+    paddingLeft: `calc(${theme.spacing.lg} * 2)`,
+
+    '&:hover': {
+      boxShadow: theme.shadows.md,
+      transform: 'scale(1.02)',
+    },
+
+    '&::before': {
+      content: '""',
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      width: rem(6),
+      backgroundImage: theme.fn.linearGradient(
+        0,
+        theme.colors.pink[6],
+        theme.colors.orange[6]
+      ),
+    },
+  },
+}));
 
 export const Events = () => {
+  const { classes } = useStyles();
+
   return (
-    <Paper radius={'md'} withBorder>
-      <Text weight={600} size="md" py="10px" pl={'20px'} color="gray">
+    <Paper withBorder radius="md" className={classes.card}>
+      <Title size="xl" weight={500} mt="xs">
         Recent Events
-      </Text>
-      <hr
-        color="#E9ECEF"
-        style={{ border: 'none', height: '1px', margin: 0, padding: 0 }}
-      />
-      <Flex direction={'column'} gap="xs" py="15px" pl={'20px'}>
-        <Anchor style={{ textDecoration: 'none' }} size={'sm'} weight={800}>
-          AMU GDSC Hakathon
+      </Title>
+      <Flex direction={'column'} py="8px" pr={'sm'} sx={{ gap: '10px' }}>
+        <Anchor style={{ textDecoration: 'none' }} size={'sm'} weight={400}>
+          AMU Computer Scoiety Hakathon
         </Anchor>
-        <Anchor style={{ textDecoration: 'none' }} size={'sm'} weight={800}>
+
+        <Anchor style={{ textDecoration: 'none' }} size={'sm'} weight={400}>
           ASAAS 2k23
         </Anchor>
       </Flex>
--- client/src/Components/TrendingPosts.tsx
+++ client/src/Components/TrendingPosts.tsx
@@ -71,32 +71,3 @@ export const TrendingPosts = () => {
     </Paper>
   );
 };
-
-<Paper radius={'md'} withBorder>
-  <Text weight={600} size="md" py="10px" pl={'20px'} color="gray">
-    Trending Posts
-  </Text>
-  <hr
-    color="#E9ECEF"
-    style={{ border: 'none', height: '1px', margin: 0, padding: 0 }}
-  />
-  <Flex direction={'column'} py="8px" pl={'20px'} sx={{ gap: '10px' }}>
-    <div>
-      <Text fz="xs" size={'xs'} tt="uppercase" fw={700} c="dimmed">
-        #1
-      </Text>
-      <Anchor style={{ textDecoration: 'none' }} size={'sm'} weight={800}>
-        Today i found a good way to achieve my practical...
-      </Anchor>
-    </div>
-
-    <div>
-      <Text fz="xs" size={'xs'} tt="uppercase" fw={700} c="dimmed">
-        #2
-      </Text>
-      <Anchor style={{ textDecoration: 'none' }} size={'sm'} weight={800}>
-        Asslamualikum dears hope you are good, i have something...
-      </Anchor>
-    </div>
-  </Flex>
-</Paper>;
